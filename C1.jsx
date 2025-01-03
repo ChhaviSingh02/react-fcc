fdf69c255984fae38b0ff4508740a05eadb4993d
chhavi <122530834+ChhaviSingh02@users.noreply.github.com>
2025-01-04 00:12:04 +0530

    Create C1.jsx

---
+++ C1.jsx
@@ -0,0 +1,11 @@
+// challenge 1: set up a reat app from the scratch again
+// this time try rendering an unordered list with 2-3 list items inside 
+// with why you are excited to be learning react
+import {createRoot} from "react-dom/client";
+createRoot(document.getElementById("root")).render(
+<ul>Snacks
+<li>Lays</li>
+<li>Kurkure</li>
+<li>Oats</li>
+</ul>
+)
